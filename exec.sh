a6bb1773748ca280cfce13a7cda15ffa82434525
Abe Winter <abe-winter@users.noreply.github.com>
2023-08-28 00:17:22 -0400

    virtualenv setup working locally

--- exec.sh
+++ exec.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
-cd `dirname $0`
+
+# bash safe mode. look at `set --help` to see what these are doing
 set -euxo pipefail 
-# python -m venv env
-# source env/bin/activate
-sudo apt-get update
-sudo apt-get install -y python3-pip
-pip3 install -r requirements.txt
+
+./setup.sh
+source .env
+
+cd $MODULE_DIR
+ENTRYPOINT=src.main
 
 # Be sure to use `exec` so that termination signals reach the python process,
 # or handle forwarding termination signals manually
-exec python3 -m src.main $@
-# deactivate
+exec $PYTHON -m $ENTRYPOINT $@
