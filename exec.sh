6c53dc56e62301342497ce62ad03d137717c676a
Abe Winter <abe-winter@users.noreply.github.com>
2023-08-28 01:32:33 -0400

    restore multi-arch, correct setup.sh

--- exec.sh
+++ exec.sh
@@ -3,12 +3,10 @@
 # bash safe mode. look at `set --help` to see what these are doing
 set -euxo pipefail 
 
-./setup.sh
 source .env
-
 cd $MODULE_DIR
-ENTRYPOINT=src.main
+./setup.sh
 
 # Be sure to use `exec` so that termination signals reach the python process,
 # or handle forwarding termination signals manually
-exec $PYTHON -m $ENTRYPOINT $@
+exec $PYTHON -m src.main $@
